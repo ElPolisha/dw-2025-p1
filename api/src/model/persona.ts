b4941c45d49bb9c6077bc08ea413ffd25dbcdf42
ElPolisha <lautaro.arrigoni@correo.ucu.edu.uy>
2025-10-03 21:21:59 -0300

    ns

--- api/src/model/persona.ts
+++ api/src/model/persona.ts
@@ -5,6 +5,7 @@ export const Persona = Type.Object({
   id_persona: Type.Integer(),
   username: Type.String({ maxLength: 15 }),
   roles: Type.Array(Type.String({ enum: ["admin", "normal"] })),
+  tareas: Type.Array(Type.String({ enum: ["tareas"]}))
 });
 
 export type Persona = Static<typeof Persona>;
--- api/src/routes/personas/_id_persona/tareas/personas-id_persona-tareas-routes.ts
+++ api/src/routes/personas/_id_persona/tareas/personas-id_persona-tareas-routes.ts
@@ -1,7 +1,69 @@
 import type { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
-
+import { Persona } from "../../../../model/persona.ts";
+import { Type } from "@fastify/type-provider-typebox";
+import { findPersonById } from "../../../../services/personas.ts";
+import { updatePerson, deletePersonById } from "../../../../services/personas.ts";
 const personasRoutes: FastifyPluginAsyncTypebox = async function (fastify) {
-  //TODO:
-};
+fastify.get(
+    "/",
+    {
+      schema: {
+        tags: ["tareas"],
+        params: Type.Pick(Persona, ["id_persona"]),
+        response: {
+          200: Persona,
+        },
+      },
+    },
+    async function (req, rep) {
+      return findPersonById(req.params.id_persona);
+    }
+  );
 
+  fastify.put(
+      "/",
+      {
+        schema: {
+          tags: ["tareas"],
+          params: Type.Pick(Persona, ["id_persona"]),
+          body: Type.Pick(Persona, ["username", "roles"], {
+            examples: [
+              {
+                username: "userCambiado",
+                roles: ["normal"],
+              },
+            ],
+          }),
+          response: {
+            204: Type.Null(),
+          },
+        },
+      },
+      async function (req, rep) {
+        rep.code(204);
+        return updatePerson(
+          req.params.id_persona,
+          req.body.username,
+          req.body.roles
+        );
+      }
+    );
+
+  fastify.delete(
+    "/",
+    {
+      schema: {
+        tags: ["tareas"],
+        params: Type.Pick(Persona, ["id_persona"]),
+        response: {
+          204: Type.Null(),
+        },
+      },
+    },
+    async function (req, rep) {
+      rep.code(204);
+      return deletePersonById(req.params.id_persona);
+    }
+  );
+};
 export default personasRoutes;
